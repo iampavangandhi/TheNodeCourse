bfcef180d4abe2a46779eaadead70fea24a63f17
Pavan Gandhi <pavangandhi100@gmail.com>
2020-07-13 20:30:09 +0530

    Day33

--- 04 Auth and Database/Auth Topic2/Code/index.js
+++ 04 Auth and Database/Auth Topic2/Code/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const { signIn, welcome, refresh } = require('./handlers')
+const { signIn, welcome, refresh } = require("./handlers");
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cookieParser())
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.post('/signin', signIn)
-app.get('/welcome', welcome)
-app.post('/refresh', refresh)
+app.post("/signin", signIn);
+app.get("/welcome", welcome);
+app.post("/refresh", refresh);
 
-app.listen(8000)
+app.listen(8000, () => {
+  console.log("Listening at Port:8000");
+});
